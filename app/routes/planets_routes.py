5e1d906ae9767965dd6f12f46ddbeea84f016a33
Esther Kang <estherkang.j@gmail.com>
2025-04-18 11:27:17 -0700

    adds error handling

--- app/routes/planets_routes.py
+++ app/routes/planets_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, abort, make_response
 from ..models.planet import planets
 
 planets_bp = Blueprint("planets_bp", __name__, url_prefix="/planets")
@@ -18,17 +18,31 @@ def get_all_planets():
         )
     return planets_response
 
-@planets_bp.get("/<id>")
 
+@planets_bp.get("/<id>")
 def get_planets_by_id(id):
-    id = int(id)
+    planet = validate_planet(id)
+
+    planet_dict = dict(
+        id = planet.id,
+        name = planet.name,
+        description = planet.description,
+        moons_count = planet.moons_count
+    )
+
+    return planet_dict
+
+
+def validate_planet(id):
+    try:
+        id = int(id)
+    except ValueError:
+        response = {"message": f"Planet id {id} is invalid"}
+        abort(make_response(response, 400))
 
     for planet in planets:
         if planet.id == id:
-            planet_dict = dict(
-                id = planet.id,
-                name = planet.name,
-                description = planet.description,
-                moons_count = planet.moons_count
-            )
-            return planet_dict
+            return planet
+
+    response = {"message": f"Planet with id:{id} not found"}
+    abort(make_response(response, 404))
