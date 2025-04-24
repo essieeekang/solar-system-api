7847625d4900ba61d79f5bbd48b7fc0d2e4624f4
Collette <cece554@gmail.com>
2025-04-24 14:26:26 -0600

    add routes - wave 3

--- app/routes/planets_routes.py
+++ app/routes/planets_routes.py
@@ -1,48 +1,88 @@
-from flask import Blueprint, abort, make_response
-# from ..models.planet import planets
+from flask import Blueprint, abort, make_response, request
+from ..db import db
+from app.models.planet import Planet
+
 
 planets_bp = Blueprint("planets_bp", __name__, url_prefix="/planets")
 
 
+@planets_bp.post("")
+
+def create_planet():
+    request_body = request.get_json()
+    name = request_body["name"]
+    description = request_body["description"]
+    moons_count = request_body["moons_count"]
+
+    new_planet = Planet(name=name, description=description, moons_count=moons_count)
+    db.session.add(new_planet)
+    db.session.commit()
+
+    response = {
+        "id": new_planet.id,
+        "name": new_planet.name,
+        "description": new_planet.description,
+        "moons_count": new_planet.moons_count
+    }
+    return response, 201
+
 @planets_bp.get("")
 def get_all_planets():
+
+    query = db.select(Planet).order_by(Planet.id)
+    planets = db.session.scalars(query)
+
     planets_response = []
+
     for planet in planets:
         planets_response.append(
             {
                 "id": planet.id,
-                "name": planet.name, 
+                "name": planet.name,
                 "description": planet.description,
-                "moons_count": planet.moons_count
+                "moons_count": planet.moons_count,
             }
         )
     return planets_response
+    
+# def get_all_planets():
+#     planets_response = []
+#     for planet in planets:
+#         planets_response.append(
+#             {
+#                 "id": planet.id,
+#                 "name": planet.name, 
+#                 "description": planet.description,
+#                 "moons_count": planet.moons_count
+#             }
+#         )
+#     return planets_response
 
 
-@planets_bp.get("/<id>")
-def get_planets_by_id(id):
-    planet = validate_planet(id)
+# @planets_bp.get("/<id>")
+# def get_planets_by_id(id):
+#     planet = validate_planet(id)
 
-    planet_dict = dict(
-        id = planet.id,
-        name = planet.name,
-        description = planet.description,
-        moons_count = planet.moons_count
-    )
+#     planet_dict = dict(
+#         id = planet.id,
+#         name = planet.name,
+#         description = planet.description,
+#         moons_count = planet.moons_count
+#     )
 
-    return planet_dict
+#     return planet_dict
 
 
-def validate_planet(id):
-    try:
-        id = int(id)
-    except ValueError:
-        response = {"message": f"Planet id {id} is invalid"}
-        abort(make_response(response, 400))
+# def validate_planet(id):
+#     try:
+#         id = int(id)
+#     except ValueError:
+#         response = {"message": f"Planet id {id} is invalid"}
+#         abort(make_response(response, 400))
 
-    for planet in planets:
-        if planet.id == id:
-            return planet
+#     for planet in planets:
+#         if planet.id == id:
+#             return planet
 
-    response = {"message": f"Planet with id:{id} not found"}
-    abort(make_response(response, 404))
+#     response = {"message": f"Planet with id:{id} not found"}
+#     abort(make_response(response, 404))
