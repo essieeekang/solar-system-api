3471553639ea12a83769c50bf8ba20a6a678667b
Esther Kang <estherkang.j@gmail.com>
2025-04-24 11:37:28 -0700

    fixes moons_count data type from bigint to int

--- app/models/planet.py
+++ app/models/planet.py
@@ -7,4 +7,4 @@ class Planet(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     name: Mapped[str]
     description: Mapped[str] = mapped_column(Text)
-    moons_count: Mapped[int] = mapped_column(BigInteger)
+    moons_count: Mapped[int]
